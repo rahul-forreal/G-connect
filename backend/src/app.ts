412074912dc7c85fc7a2db74b08e5801036b214a
Rahul Mahendrakar <rahulm31304@gmail.com>
2026-07-03 17:56:29 +0530

    feature: express server setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,9 @@
 import express from "express";
  const app=express();
  app.use(express.json());
+ app.get("/",(req,res)=>{
+    res.json({
+        message:"Graph recommendation engine API"
+    });
+ });
  export default app;
