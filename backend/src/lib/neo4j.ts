b186f0208a37db7679f73aa51c4d00a3a19dd7d4
Rahul Mahendrakar <rahulm31304@gmail.com>
2026-07-15 14:43:03 +0530

    feature: containerize backend with PostgreSQL and Neo4j

--- backend/src/lib/neo4j.ts
+++ backend/src/lib/neo4j.ts
@@ -1,11 +1,12 @@
 import neo4j from "neo4j-driver";
 
+const uri = process.env.NEO4J_URI || "bolt://localhost:7687";
+const user = process.env.NEO4J_USER || "neo4j";
+const password = process.env.NEO4J_PASSWORD || "password";
+
 const driver = neo4j.driver(
-    "bolt://localhost:7687",
-    neo4j.auth.basic(
-        "neo4j",
-        "password"
-    )
+    uri,
+    neo4j.auth.basic(user, password)
 );
 
 export default driver;
